9dc8163b59815755c634ad0f55f1608f59a70310
Pablouc <lpablouc9gmail.com>
2022-09-19 20:16:36 -0600

    read and write in cache functions

--- processor/Controller.py
+++ processor/Controller.py
@@ -11,19 +11,24 @@ class Controller:
   def mesiRead(self, inst, cachesArray):
     dir=inst[2]
     searchResult=self.cpu.searchInCache(dir)
-    if (searchResult!=False):
-      self.cpu.readCache()#--------------->To Develop
-    searchResult=self.cpu.searchInForeingCaches(cachesArray,dir)
-    if (searchResult!=False):
+    #Mi cache tiene la dirección y el estado no es invalido
+    if ((searchResult!=False) & (searchResult[1]!="I")):
+      self.cpu.readCache()
+    foreignSearch=self.cpu.searchInForeingCaches(cachesArray,dir)
+    #Otra cache tiene la dirección
+    if (foreignSearch!=False):
       myBlockToDo=["WRITE","S", ""]
+      #Posibles estados de los bloques a leer
+      match foreignSearch[2]:
+        case "M": foreingBlockToDo=["READ","S", "WB"]; return [myBlockToDo,foreignSearch,foreingBlockToDo]
+        case "E": foreingBlockToDo=["READ","S",""]; return [myBlockToDo,foreignSearch,foreingBlockToDo]
+        case "S": foreingBlockToDo=["READ","S",""]; return [myBlockToDo,foreignSearch,foreingBlockToDo]
 
-      match searchResult[2]:
-        case "M": foreingBlockToDo=["READ","S", "WB"]; return [myBlockToDo,searchResult,foreingBlockToDo]
-        case "E": foreingBlockToDo=["READ","S",""]; return [myBlockToDo,searchResult,foreingBlockToDo]
-
+    #Nadie lo tiene y voy a leer de memoria
     else:
         myBlockToDo=["WRITE","S", "WB"]
         return [myBlockToDo, searchResult, [""]]
+
   
   def mesiWrite(self, inst, cachesArray):
     dir=inst[2]
@@ -34,7 +39,7 @@ class Controller:
       foreingBlockToDo=["","I", "checkWB"]
       #Cuando el bloque la dirección la tenemos nosotros y alguien mas
       if(foreignSearch!=False):
-        return [myBlockToDo,searchResult,foreingBlockToDo]#Aqui debe checkear si debe hacer WB(en caso de que este en M)
+        return [myBlockToDo,foreignSearch,foreingBlockToDo]#Aqui debe checkear si debe hacer WB(en caso de que este en M)
       #Cuando solo nosotros tenemos la dirección
       else:
         self.cpu.writeCache(searchResult[1],dir,inst[3],"M")
--- processor/cpu.py
+++ processor/cpu.py
@@ -2,15 +2,39 @@ from cache import Cache
 
 
 class Cpu:
+    #Replacement policy: LIFO
+    lastBlockChanged=0
 
     def __init__(self) :
        self.cache=Cache()
+    #When block is empty it means that a replacement is required
+    def writeCache(self, block,dir, data, stateToChange):
+        cache=self.cache
+        if(block==""):
+            match self.lastBlockChanged:
+                case 1: cache.block1.dir=dir; cache.block1.state= stateToChange; cache.block1.data=data
+                case 2: cache.block2.dir=dir; cache.block2.state= stateToChange; cache.block2.data=data
+                case 3: cache.block3.dir=dir; cache.block3.state= stateToChange; cache.block3.data=data
+                case 4: cache.block4.dir=dir; cache.block4.state= stateToChange; cache.block4.data=data
+        else:
+            match block:
+                case 1:  cache.block1.state= stateToChange; cache.block1.data=data; self.lastBlockChanged=1
+                case 2:  cache.block2.state= stateToChange; cache.block2.data=data; self.lastBlockChanged=2
+                case 3:  cache.block3.state= stateToChange; cache.block3.data=data; self.lastBlockChanged=3
+                case 4:  cache.block4.state= stateToChange; cache.block4.data=data; self.lastBlockChanged=4
 
-    def writeCache(self, block,dir, data, state):
-        pass
 
-    def readCache(self, dir, data):
-        pass
+    #Reads the cache
+    #Receive the blocks number and the address
+    # Returns [block's number, state, address, data]               
+    def readCache(self, block, dir):
+        cache=self.cache
+        match block:
+                case 1:  state= cache.block1.state; data=cache.block1.data 
+                case 2:  state= cache.block2.state; data=cache.block2.data 
+                case 3:  state= cache.block3.state; data=cache.block3.data 
+                case 4:  state= cache.block4.state; data=cache.block4.data
+        return[block, state, dir, data]
 
     def changeBlockState(self, state, block):
         match block:
@@ -44,7 +68,8 @@ class Cpu:
         blockNum=1
         for x in caches:
             for y in x: #y = blocks
-                if (y[1]==bin(dir)):
+                #verifico que tenga la dirección que busco y que es estado no sea inválido
+                if ((y[1]==bin(dir)) & (y[0]!="I")):
                     return[cacheNum,blockNum,y[0], dir]
                 blockNum=blockNum+1
             cacheNum=cacheNum+1
