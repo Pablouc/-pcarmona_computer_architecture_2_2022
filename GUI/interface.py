088473927466dcb245ed8d2f7d4135ecd377598a
Pablouc <lpablouc9gmail.com>
2022-09-22 16:15:23 -0600

    layout finished 1.1

--- GUI/interface.py
+++ GUI/interface.py
@@ -23,9 +23,9 @@ def generateGrid_Core1():
             if(i==0):
                 if(j>0):
                     match j:
-                        case 1: name="State"
-                        case 2: name="Address"
-                        case 3: name="Data"
+                        case 1: name="Estado"
+                        case 2: name="Dirección"
+                        case 3: name="Dato"
                     label = tk.Label(master=gridFrame1, text=name, width=10, height=2)
                     label.pack()
                     continue
@@ -46,9 +46,9 @@ def generateGrid_Core2():
             if(i==0):
                 if(j>0):
                     match j:
-                        case 1: name="State"
-                        case 2: name="Address"
-                        case 3: name="Data"
+                        case 1: name="Estado"
+                        case 2: name="Dirección"
+                        case 3: name="Dato"
                     label = tk.Label(master=gridFrame2, text=name, width=10, height=2)
                     label.pack()
                     continue
@@ -70,9 +70,9 @@ def generateGrid_Core3():
             if(i==0):
                 if(j>0):
                     match j:
-                        case 1: name="State"
-                        case 2: name="Address"
-                        case 3: name="Data"
+                        case 1: name="Estado"
+                        case 2: name="Dirección"
+                        case 3: name="Dato"
                     label = tk.Label(master=gridFrame3, text=name, width=10, height=2)
                     label.pack()
                     continue
@@ -93,9 +93,9 @@ def generateGrid_Core4():
             if(i==0):
                 if(j>0):
                     match j:
-                        case 1: name="State"
-                        case 2: name="Address"
-                        case 3: name="Data"
+                        case 1: name="Estado"
+                        case 2: name="Dirección"
+                        case 3: name="Dato"
                     label = tk.Label(master=gridFrame4, text=name, width=10, height=2)
                     label.pack()
                     continue
@@ -107,10 +107,31 @@ def generateGrid_Core4():
              
             label = tk.Label(master=gridFrame4, text=f"0", width=8, height=2)
             label.pack()
-        
+
+def generateGrid_Mem():
+    for i in range(9):
+        for j in range(3):
+            gridFrameMem = tk.Frame(master=frame_Mem,relief=tk.RAISED,borderwidth=1)
+            gridFrameMem.grid(row=i, column=j, padx=5, pady=5)
+            if(i==0):
+                if(j>0):
+                    match j:
+                        case 1: name="Dirección"
+                        case 2: name="Dato"
+                    label = tk.Label(master=gridFrameMem, text=name, width=10, height=2)
+                    label.pack()
+                    continue
+            if(i>0 ):
+                if(j==0):
+                    label = tk.Label(master=gridFrameMem, text=f"Bloque {i}", width=10, height=2)
+                    label.pack()
+                    continue
+             
+            label = tk.Label(master=gridFrameMem, text=f"0", width=8, height=2)
+            label.pack()      
 
 window =tk.Tk()
-window.geometry("1600x400")
+window.geometry("1200x700")
 
 
 #Top frame with the menu
@@ -122,94 +143,127 @@ generateGrid_Core1()
 
 
 frame_Core2=tk.Frame(master=window, relief=tk.RAISED, borderwidth=5)
-gridFrame_Core2 = tk.Frame(master=frame_Core2,relief=tk.RAISED,borderwidth=1)
 generateGrid_Core2()
 
 frame_Core3=tk.Frame(master=window, relief=tk.RAISED, borderwidth=5)
-gridFrame_Core3 = tk.Frame(master=frame_Core3,relief=tk.RAISED,borderwidth=1)
 generateGrid_Core3()
 
 frame_Core4=tk.Frame(master=window, relief=tk.RAISED, borderwidth=5)
-gridFrame_Core4 = tk.Frame(master=frame_Core4,relief=tk.RAISED,borderwidth=1)
 generateGrid_Core4()
 
+frame_Mem=tk.Frame(master=window, relief=tk.RAISED, borderwidth=5)
+generateGrid_Mem()
+
 #Creating the menu
-btn_Continuous=tk.Button(master=frame_Menu,text="Continuo")
+btn_Continuous=tk.Button(master=frame_Menu,text="Continuo", font=(25))
 btn_Continuous.pack(side=tk.LEFT,expand=True)
 btn_Continuous.bind('<Button-1>', continuous_Flag)
 
-btn_Pause=tk.Button(master=frame_Menu, text="Pausa", bg="red")
+btn_Pause=tk.Button(master=frame_Menu, text="Pausa", bg="red", font=(25))
 btn_Pause.pack( side=tk.LEFT,expand=True)
 btn_Pause.bind('<Button-1>', pause_Flag)
 
-btn_Step_by_Step=tk.Button(master=frame_Menu, text="Paso a paso")
+btn_Step_by_Step=tk.Button(master=frame_Menu, text="Paso a paso", font=(25))
 btn_Step_by_Step.pack(side=tk.LEFT,expand=True)
 btn_Step_by_Step.bind('<Button-1>', step_By_Step_Flag)
 
-lbl_Inst = tk.Label(master=frame_Menu, text="Instrucción")
+lbl_Inst = tk.Label(master=frame_Menu, text="Instrucción", font=(25))
 lbl_Inst.pack( side=tk.LEFT,expand=True)
 
-input_Inst = tk.Entry(master=frame_Menu)
+input_Inst = tk.Entry(master=frame_Menu, font=(25))
 input_Inst.pack( side=tk.LEFT,expand=True)
 #To get input_Inst data
 # entry.get()
 
-btn_sendInst=tk.Button(master=frame_Menu,text="Enviar") 
+btn_sendInst=tk.Button(master=frame_Menu,text="Enviar", font=(25)) 
 btn_sendInst.pack(side=tk.LEFT,expand=True)
 btn_sendInst.bind('<Button-1>', send_Flag)
 
 #Processor 1 layout
 
 lbl_Core1 = tk.Label(master=frame_Core1, text="CORE 1", font="bold")
-lbl_Core1.grid()
+lbl_Core1.grid(row=5, column=0)
 lbl_Core1.config(bg="yellow")
 
-lbl_Core1_Wmiss = tk.Label(master=frame_Core1, text="write miss")
-lbl_Core1_Wmiss.grid()
+lbl_Core1_Wmiss = tk.Label(master=frame_Core1, relief=tk.RAISED,text="write miss" )
+lbl_Core1_Wmiss.grid(row=5, column=1)
 
-lbl_Core1_Rmiss = tk.Label(master=frame_Core1, text="read miss")
-lbl_Core1_Rmiss.grid()
+lbl_Core1_Rmiss = tk.Label(master=frame_Core1,relief=tk.RAISED, text="read miss")
+lbl_Core1_Rmiss.grid(row=5, column=2)
+
+lbl_Core1_Inst = tk.Label(master=frame_Core1,relief=tk.RAISED, text="Instrucción", font="bold")
+lbl_Core1_Inst.grid(row=6, column=0)
+
+lbl_Core1_InstData = tk.Label(master=frame_Core1,relief=tk.RAISED, text="", font="bold")
+lbl_Core1_InstData.grid(row=6, column=1)
 
 #Processor 2 layout
 lbl_Core2 = tk.Label(master=frame_Core2, text="CORE 2",font="bold")
-lbl_Core2.grid()
+lbl_Core2.grid(row=5, column=0)
 lbl_Core2.config(bg="yellow")
 
-lbl_Core2_Wmiss = tk.Label(master=frame_Core2, text="write miss")
-lbl_Core2_Wmiss.grid()
+lbl_Core2_Wmiss = tk.Label(master=frame_Core2,  relief=tk.RAISED,text="write miss",font="bold" )
+lbl_Core2_Wmiss.grid(row=5, column=1)
 
-lbl_Core2_Rmiss = tk.Label(master=frame_Core2, text="read miss")
-lbl_Core2_Rmiss.grid()
+lbl_Core2_Rmiss = tk.Label(master=frame_Core2,relief=tk.RAISED, text="read miss", font="bold")
+lbl_Core2_Rmiss.grid(row=5, column=2)
+
+lbl_Core2_Inst = tk.Label(master=frame_Core2,relief=tk.RAISED, text="Instrucción", font="bold")
+lbl_Core2_Inst.grid(row=6, column=0)
+
+lbl_Core2_InstData = tk.Label(master=frame_Core2,relief=tk.RAISED, text="", font="bold")
+lbl_Core2_InstData.grid(row=6, column=1)
 
 
 #Processor 3 layout
 lbl_Core3 = tk.Label(master=frame_Core3, text="CORE 3",font="bold")
-lbl_Core3.grid()
+lbl_Core3.grid(row=5, column=0)
 lbl_Core3.config(bg="yellow")
 
-lbl_Core3_Wmiss = tk.Label(master=frame_Core3, text="write miss")
-lbl_Core3_Wmiss.grid()
+lbl_Core3_Wmiss = tk.Label(master=frame_Core3,  relief=tk.RAISED,text="write miss",font="bold" )
+lbl_Core3_Wmiss.grid(row=5, column=1)
 
-lbl_Core3_Rmiss = tk.Label(master=frame_Core3, text="read miss")
-lbl_Core3_Rmiss.grid()
+lbl_Core3_Rmiss = tk.Label(master=frame_Core3, relief=tk.RAISED, text="read miss", font="bold")
+lbl_Core3_Rmiss.grid(row=5, column=2)
+
+lbl_Core3_Inst = tk.Label(master=frame_Core3,relief=tk.RAISED, text="Instrucción", font="bold")
+lbl_Core3_Inst.grid(row=6, column=0)
+
+lbl_Core3_InstData = tk.Label(master=frame_Core3,relief=tk.RAISED, text="", font="bold")
+lbl_Core3_InstData.grid(row=6, column=1)
 
 
 #Processor 4 layout
 lbl_Core4 = tk.Label(master=frame_Core4, text="CORE 4",font="bold")
-lbl_Core4.grid()
+lbl_Core4.grid(row=5, column=0)
 lbl_Core4.config(bg="yellow")
 
-lbl_Core4_Wmiss = tk.Label(master=frame_Core4, text="write miss")
-lbl_Core4_Wmiss.grid()
+lbl_Core4_Wmiss = tk.Label(master=frame_Core4,  relief=tk.RAISED,text="write miss",font="bold" )
+lbl_Core4_Wmiss.grid(row=5, column=1)
 
-lbl_Core4_Rmiss = tk.Label(master=frame_Core4, text="read miss")
-lbl_Core4_Rmiss.grid()
+lbl_Core4_Rmiss = tk.Label(master=frame_Core4, relief=tk.RAISED, text="read miss", font="bold")
+lbl_Core4_Rmiss.grid(row=5, column=2)
 
-frame_Menu.pack(fill=tk.BOTH, side=tk.TOP,expand=True)
-frame_Core1.pack(fill=tk.BOTH, side=tk.LEFT,expand=True)
-frame_Core2.pack(fill=tk.BOTH, side=tk.LEFT,expand=True)    
-frame_Core3.pack(fill=tk.BOTH, side=tk.LEFT,expand=True)
-frame_Core4.pack(fill=tk.BOTH, side=tk.LEFT,expand=True)
+lbl_Core4_Inst = tk.Label(master=frame_Core4,relief=tk.RAISED, text="Instrucción", font="bold")
+lbl_Core4_Inst.grid(row=6, column=0)
+
+lbl_Core4_InstData = tk.Label(master=frame_Core4,relief=tk.RAISED, text="", font="bold")
+lbl_Core4_InstData.grid(row=6, column=1)
+
+#Memory layout
+lbl_Mem = tk.Label(master=frame_Mem, text="Memory",font="bold")
+lbl_Mem.grid()
+lbl_Mem.config(bg="yellow")
+
+
+
+frame_Core1.grid(row=1, column=0, padx=10, pady=15)
+frame_Core2.grid(row=1, column=1, padx=10, pady=15)
+frame_Core3.grid(row=2, column=0, padx=10, pady=10)
+frame_Core4.grid(row=2, column=1, padx=10, pady=10)
+frame_Mem.grid(row=1, column=3, rowspan=2, padx=20)
+
+frame_Menu.grid(row=0, column=0, columnspan=2)
 
 
 
